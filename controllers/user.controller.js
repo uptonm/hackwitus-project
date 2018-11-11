13869a93954b94619d96c1912090070beccab1f6
Mike Upton <uptonm@wit.edu>
2018-11-10 20:46:11 -0500

    Added routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,3 +17,31 @@ exports.post = async (req, res) => {
   const user = await new User(req.body).save();
   return res.status(200).send({ user });
 };
+
+exports.put = async (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const exists = await User.findById(req.params.id);
+    if (exists) {
+      await User.findByIdAndUpdate(req.params.id, req.body);
+      return res.status(200).send({ update: req.body });
+    } else {
+      return res.status(404).send({ error: "User not found" });
+    }
+  } else {
+    return res.status(400).send({ error: "Id not valid" });
+  }
+};
+
+exports.delete = async (req, res) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const exists = await User.findById(req.params.id);
+    if (exists) {
+      await User.findByIdAndDelete(req.params.id);
+      return res.status(200).send({ deleted: req.params.id });
+    } else {
+      return res.status(404).send({ error: "User not found" });
+    }
+  } else {
+    return res.status(400).send({ error: "Id not valid" });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,4 +3,6 @@ const controller = require("../controllers/user.controller");
 module.exports = app => {
   app.get("/users/:id", controller.get);
   app.post("/users", controller.post);
+  app.put("/users/:id", controller.put);
+  app.delete("/users/:id", controller.delete);
 };
